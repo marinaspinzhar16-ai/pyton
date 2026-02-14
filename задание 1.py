7bc7b3a809a0fabb4aa970a47b6ba18a5dcee977
marinaspinzhar16-ai <marinaspinzhar16@gmail.com>
2026-02-14 14:55:52 +0100

    Update задание 1.py

--- задание 1.py
+++ задание 1.py
@@ -1,6 +1,6 @@
 # https://github.com/marinaspinzhar16-ai/python
 from datetime import datetime, date
-datetime
+
 
 def get_days_from_today(date_str: str) -> int:
     """
@@ -10,18 +10,14 @@ def get_days_from_today(date_str: str) -> int:
     :return: кількість днів (від'ємне число, якщо дата в майбутньому)
     """
     try:
-        given_date: date = datetime.strptime(date_str, "%Y-%m-%d").date()
-        today: date = date.today()
-
+        given_date = datetime.strptime(date_str, "%Y-%m-%d").date()
+        today = date.today()
         return (today - given_date).days
-
     except ValueError:
         raise ValueError("Невірний формат дати. Очікується 'YYYY-MM-DD'")
 
 
-
 if __name__ == "__main__":
-    example_date: str = "2021-10-09"
-    result: int = get_days_from_today(example_date)
-
+    example_date = "2021-10-09"
+    result = get_days_from_today(example_date)
     print(f"Різниця в днях: {result}")
